e807e19545909d7897c114db5399ad9ac01cf8e3
AhmedSalman5 <ahmedsalman1200011@gmail.com>
2024-02-09 18:11:32 +0200

    new

--- lib/modules/on_boarding/on_boarding_screen.dart
+++ lib/modules/on_boarding/on_boarding_screen.dart
@@ -9,18 +9,43 @@ class OnBoardingScreen extends StatelessWidget {
       appBar: AppBar(),
       body: Column(
         children: [
-          // PageView.builder(
-          //   itemBuilder: (context , index)=>
-          //   ),
-          Image(image: AssetImage('assets/images/onboard_1.jpg'),),
-          const SizedBox(
-            height: 30,
-          ),
-          Text(
-            'Screen Title',
+          PageView.builder(
+            itemBuilder: (context , index)=> buildBoardItem(),
+            itemCount: 3,
             ),
         ],
       ),
     );
   }
+
+
+
+  Widget buildBoardItem ()=> const Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Expanded(
+            child: Image(
+              image: AssetImage('assets/images/onboard_1.jpg'),)),
+          SizedBox(
+            height: 30,
+          ),
+          Text(
+            'Screen Title',
+            style: TextStyle(
+              fontSize: 24.0,
+              fontWeight: FontWeight.bold,
+            ),
+            ),
+          SizedBox(
+            height: 15,
+          ),
+          Text(
+            'Screen Body',
+            style: TextStyle(
+              fontSize: 14.0,
+              fontWeight: FontWeight.bold,
+            ),
+            ),
+        ],
+      );
 }
