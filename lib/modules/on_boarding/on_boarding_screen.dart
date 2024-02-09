89b373ed0cb444d1c28969bed08e91ea2e387cb9
AhmedSalman5 <ahmedsalman1200011@gmail.com>
2024-02-09 17:42:46 +0200

    Shop App

---
+++ lib/modules/on_boarding/on_boarding_screen.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class OnBoardingScreen extends StatelessWidget {
+  const OnBoardingScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Column(
+        children: [
+          // PageView.builder(
+          //   itemBuilder: (context , index)=>
+          //   ),
+          Image(image: AssetImage('assets/images/onboard_1.jpg'),),
+          const SizedBox(
+            height: 30,
+          ),
+          Text(
+            'Screen Title',
+            ),
+        ],
+      ),
+    );
+  }
+}
