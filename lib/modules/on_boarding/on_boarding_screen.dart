56264f1b817aeda2feae7971ce1cd7a25a85bac2
AhmedSalman5 <ahmedsalman1200011@gmail.com>
2024-02-10 17:03:10 +0200

    On Boarding

--- lib/modules/on_boarding/on_boarding_screen.dart
+++ lib/modules/on_boarding/on_boarding_screen.dart
@@ -1,51 +1,168 @@
 import 'package:flutter/material.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
-class OnBoardingScreen extends StatelessWidget {
+import '../shop_login/login_screen.dart';
+
+class BoardingModel {
+  final String image;
+  final String title;
+  final String body;
+
+  BoardingModel({
+    required this.image,
+    required this.title,
+    required this.body,
+  });
+}
+
+class OnBoardingScreen extends StatefulWidget {
   const OnBoardingScreen({super.key});
+
+  @override
+  State<OnBoardingScreen> createState() => _OnBoardingScreenState();
+}
+
+class _OnBoardingScreenState extends State<OnBoardingScreen> {
+  final boardController = PageController();
+
+  final List<BoardingModel> boarding = [
+    BoardingModel(
+      image: 'assets/images/onboard_1.jpg',
+      title: 'On Board 1 Title',
+      body: 'On Board 1 Body',
+    ),
+    BoardingModel(
+      image: 'assets/images/onboard_1.jpg',
+      title: 'On Board 2 Title',
+      body: 'On Board 2 Body',
+    ),
+    BoardingModel(
+      image: 'assets/images/onboard_1.jpg',
+      title: 'On Board 3 Title',
+      body: 'On Board 3 Body',
+    ),
+  ];
+
+  bool isLast = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Column(
-        children: [
-          PageView.builder(
-            itemBuilder: (context , index)=> buildBoardItem(),
-            itemCount: 3,
-            ),
+      appBar: AppBar(
+        actions: [
+          TextButton(onPressed: (){
+            Navigator.pushAndRemoveUntil(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => const ShopLoginScreen(),
+                          ),
+                          (route) => false);
+          },child: const Text('SKIP')),
         ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(30.0),
+        child: Column(
+          children: [
+            Expanded(
+              child: PageView.builder(
+                physics: const BouncingScrollPhysics(),
+                controller: boardController,
+                onPageChanged: (int index) {
+                  if (index == boarding.length - 1) {
+                    setState(() {
+                      isLast = true;
+                    });
+                  } else {
+                    setState(() {
+                      isLast = false;
+                    });
+                  }
+                },
+                itemBuilder: (context, index) =>
+                    buildBoardItem(boarding[index]),
+                itemCount: boarding.length,
+              ),
+            ),
+            const SizedBox(
+              height: 40,
+            ),
+            Row(
+              children: [
+                SmoothPageIndicator(
+                  controller: boardController,
+                  effect: const ExpandingDotsEffect(
+                    dotColor: Colors.grey,
+                    activeDotColor: Colors.deepOrange,
+                    dotHeight: 10,
+                    expansionFactor: 4,
+                    dotWidth: 10,
+                    spacing: 5.0,
+                  ),
+                  count: boarding.length,
+                ),
+                const Spacer(),
+                FloatingActionButton(
+                  onPressed: () {
+                    if (isLast) {
+                      Navigator.pushAndRemoveUntil(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => const ShopLoginScreen(),
+                          ),
+                          (route) => false);
+                    } else {
+                      boardController.nextPage(
+                          duration: const Duration(
+                          milliseconds: 750,
+                          ),
+                          curve: Curves.fastLinearToSlowEaseIn);
+                    }
+                  },
+                  backgroundColor: Colors.deepOrange,
+                  child: const Icon(
+                    Icons.arrow_forward_ios,
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
 
-
-
-  Widget buildBoardItem ()=> const Column(
+  Widget buildBoardItem(BoardingModel model) => Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Expanded(
-            child: Image(
-              image: AssetImage('assets/images/onboard_1.jpg'),)),
-          SizedBox(
+              child: Image(
+            image: AssetImage(model.image),
+          )),
+          const SizedBox(
             height: 30,
           ),
           Text(
-            'Screen Title',
-            style: TextStyle(
+            model.title,
+            style: const TextStyle(
               fontSize: 24.0,
               fontWeight: FontWeight.bold,
             ),
-            ),
-          SizedBox(
+          ),
+          const SizedBox(
             height: 15,
           ),
           Text(
-            'Screen Body',
-            style: TextStyle(
+            model.body,
+            style: const TextStyle(
               fontSize: 14.0,
               fontWeight: FontWeight.bold,
             ),
-            ),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
         ],
       );
 }
